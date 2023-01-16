6775fc5250d09def6584e7d3fe6812fbcc8dc030
tsteph33 <93548574+tsteph33@users.noreply.github.com>
2023-01-16 03:45:06 -0500

    Add files via upload

---
+++ 12home.js
@@ -0,0 +1,17 @@
+var index =  document.createElement("index.html");
+document.body.appendChild(index);
+var interests = ["sports", "video games", "music", "food", "carsl"];
+document.write(interests);
+var courses = ["Advance Programming", "Senior Project", "Business Analytics", "Networking", "Organizational Behavior with Topics in Management", "Server Management"];
+document.write(courses);
+var contact = ["Email: tystephens512@gmail.com", "Phone: 202-713-3194", "Twitter: @1lilmelooo"];
+document.write(contact);
+<img src="tyler.jpg" alt="Tyler Stephens"></img>
+
+var home = document.createElement("home");
+document.body.appendChild(home);
+var interests = ["sports", "video games", "music", "food", "carsl"];
+document.write(interests);
+var courses = ["Advance Programming", "Senior Project", "Business Analytics", "Networking", "Organizational Behavior with Topics in Management", "Server Management"];
+document.write(courses);
+var contact = ["
